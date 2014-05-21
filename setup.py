b3e145dcc7beb4d70361088d8ec7cd1dcfab6112
Justin Cooper <jwcooper@gmail.com>
2014-05-21 13:46:30 -0500

    initial io client python library commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Adafruit_IO',
+    version='0.0.1',
+    author='Justin Cooper',
+    author_email='justin@adafruit.com',
+    packages=['adafruit_io'],
+    url='http://pypi.python.org/pypi/adafruit_io/',
+    license='LICENSE.txt',
+    description='IO Client library for io.adafruit.com',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
