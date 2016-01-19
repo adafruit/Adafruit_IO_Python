028013b1f1521287136b70adfc71de83269381df
Tony DiCola <tony@tonydicola.com>
2016-01-19 02:34:33 -0800

    Update setup.py for pypa publish.

--- setup.py
+++ setup.py
@@ -2,18 +2,29 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup
 
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: MacOS',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: Home Automation',
+               'Topic :: System :: Hardware']
+
 setup(
-    name='adafruit-io',
-    version='1.0.0',
-    author='Justin Cooper',
-    author_email='justin@adafruit.com',
-    packages=['Adafruit_IO'],
-    url='http://pypi.python.org/pypi/adafruit-io/',
-    license='LICENSE.txt',
-    description='IO Client library for io.adafruit.com',
-    long_description=open('README.md').read(),
-    install_requires=[
-        "requests",
-        "paho-mqtt"
-    ],
+    name             = 'adafruit-io',
+    version          = '1.0.0',
+    author           = 'Justin Cooper',
+    author_email     = 'justin@adafruit.com',
+    packages         = ['Adafruit_IO'],
+    url              = 'https://github.com/adafruit/io-client-python',
+    license          = 'MIT',
+    keywords         = 'Adafruit IO',
+    classifiers      = classifiers,
+    description      = 'Client library for Adafruit IO (http://io.adafruit.com/).',
+    long_description = open('README.md').read(),
+    install_requires = ["requests", "paho-mqtt"]
 )
