24493c648a634fff622beb98fe5fc9c88e1f3e61
brentru <robots199@me.com>
2018-06-29 16:59:35 -0400

    modified model for location feeds

---
+++ examples/api/location.py
@@ -0,0 +1,45 @@
+"""
+'location.py'
+==================================
+Example of sending location over an
+Adafruit IO feed to a Map Dashboard
+block
+
+Author(s): Brent Rubell
+"""
+
+# Import Adafruit IO REST client.
+from Adafruit_IO import Client, Feed, Data, RequestError
+import datetime
+
+# Set to your Adafruit IO key.
+ADAFRUIT_IO_USERNAME = 'YOUR_IO_USERNAME'
+ADAFRUIT_IO_KEY = 'YOUR_IO_KEY'
+
+# Create an instance of the REST client.
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+# create a location feed
+try:
+    location = aio.feeds('location')
+except RequestError:
+    feed = Feed(name="location")
+    location = aio.create_feed(feed)
+
+
+# Adafruit HQ Coordinates
+lat = 40.726190
+lon = 74.005334
+ele = 0
+
+# WIP: send_location_data implementation
+#aio.send_location_data(location.key,lat,lon,ele)
+
+# send implementation
+
+
+# send_batch_data implementation
+data_list = [Data(value=0, id='value', 
+lat = 40.726190,lon=-74.005334, ele=0)]
+aio.send_batch_data(location.key, data_list)
+
