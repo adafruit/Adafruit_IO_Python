80b455a0ebba1a02d65e4f31eb7a81b649dd2b9c
brentrubell <brentru@users.noreply.github.com>
2018-07-17 10:00:54 -0400

    TypeError Handling (#61)
    Fix TypeError on 3.5.x (RasPi, Stretch)

--- examples/api/simple.py
+++ examples/api/simple.py
@@ -3,7 +3,7 @@
 # Author: Tony DiCola
 
 # Import Adafruit IO REST client.
-from Adafruit_IO import Client
+from Adafruit_IO import Client, RequestError, Feed
 
 # Set to your Adafruit IO key.
 # Remember, your key is a secret,
@@ -17,20 +17,34 @@ ADAFRUIT_IO_USERNAME = 'YOUR_AIO_USERNAME'
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
+# Assign a foo feed, if one exists already
+try:
+    foo = aio.feeds('foo')
+except RequestError: # Doesn't exist, create a new feed
+    feed = Feed(name="foo")
+    foo = aio.create_feed(feed)
+
+# Assign a test feed, if one exists already
+try:
+    test = aio.feeds('test')
+except RequestError: # Doesn't exist, create a new feed
+    feed = Feed(name="test")
+    test = aio.create_feed(feed)
+
 # Send a value to the feed 'Test'.
-aio.send_data('Test', 42)
+aio.send_data(test.key, 42)
 
 # Send a string value 'bar' to the feed 'Foo'.
-aio.send_data('Foo', 'bar')
+aio.send_data(foo.key, 'bar')
 
 # Now read the most recent value from the feed 'Test'.  Notice that it comes
 # back as a string and should be converted to an int if performing calculations
 # on it.
-data = aio.receive('Test')
+data = aio.receive(test.key)
 print('Retrieved value from Test has attributes: {0}'.format(data))
 print('Latest value from Test: {0}'.format(data.value))
 
 # Finally read the most revent value from feed 'Foo'.
-data = aio.receive('Foo')
+data = aio.receive(foo.key)
 print('Retrieved value from Foo has attributes: {0}'.format(data))
 print('Latest value from Foo: {0}'.format(data.value))
