d1b1ac8bc335a10211142909f1e842af60c8f4c3
brentru <robots199Wme.com>
2019-01-15 15:20:30 -0500

    add subscribe_randomizer and mqtt example

--- examples/api/random_data.py
+++ examples/api/random_data.py
@@ -12,8 +12,8 @@ import json
 from Adafruit_IO import Client, Feed, RequestError
 
 # Set to your Adafruit IO key.
-ADAFRUIT_IO_USERNAME = 'USER'
-ADAFRUIT_IO_KEY = 'KEY'
+ADAFRUIT_IO_USERNAME = 'brubell'
+ADAFRUIT_IO_KEY = '6ec4b31bd2c54a09be911e0c1909b7ab'
 
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
