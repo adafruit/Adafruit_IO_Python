c628f7cb3126f5199e9cade826113eadf1c708f1
tyeth <tyethgundry@googlemail.com>
2026-02-12 19:26:43 +0000

    fix(feed_id): Cleaup feed key vs id usage

--- examples/basics/subscribe.py
+++ examples/basics/subscribe.py
@@ -6,22 +6,23 @@ Subscribes to an Adafruit IO Feed
 Author(s): Brent Rubell, Todd Treece for Adafruit Industries
 """
 # Import standard python modules.
+import os
 import sys
 
 # This example uses the MQTTClient instead of the REST client
 from Adafruit_IO import MQTTClient
 
-# Set to your Adafruit IO key.
-# Remember, your key is a secret,
-# so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = 'YOUR_AIO_KEY'
-
 # Set to your Adafruit IO username.
 # (go to https://accounts.adafruit.com to find your username)
-ADAFRUIT_IO_USERNAME = 'YOUR_AIO_USERNAME'
+ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME', 'YOUR_AIO_USERNAME')
 
-# Set to the ID of the feed to subscribe to for updates.
-FEED_ID = 'counter'
+# Set to your Adafruit IO key.
+# Remember, your key is a secret,
+# so make sure **not** to publish it when you publish this code!
+ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY', 'YOUR_AIO_KEY')
+
+# Set to the key of the feed to subscribe to for updates.
+FEED_KEY = 'counter'
 
 # Define callback functions which will be called when certain events happen.
 def connected(client):
@@ -31,20 +32,20 @@ def connected(client):
     can make calls against it easily.
     """
     # Subscribe to changes on a feed named Counter.
-    print('Subscribing to Feed {0}'.format(FEED_ID))
-    client.subscribe(FEED_ID)
+    print('Subscribing to Feed {0}'.format(FEED_KEY))
+    client.subscribe(FEED_KEY)
     print('Waiting for feed data...')
 
 def disconnected(client):
     """Disconnected function will be called when the client disconnects."""
     sys.exit(1)
 
-def message(client, feed_id, payload):
+def message(client, feed_key, payload):
     """Message function will be called when a subscribed feed has a new value.
-    The feed_id parameter identifies the feed, and the payload parameter has
+    The feed_key parameter identifies the feed, and the payload parameter has
     the new value.
     """
-    print('Feed {0} received new value: {1}'.format(feed_id, payload))
+    print('Feed {0} received new value: {1}'.format(feed_key, payload))
 
 
 # Create an MQTT client instance.
